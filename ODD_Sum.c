380f23a29992a7d0c6f205515215160bd1d5c877
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-04-26 23:49:53 +0530

    #include<stdio.h> int main() {     int n,a[100],os=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2!=0)         {             os=os+a[i];         }     }     printf("%d",os); }

---
+++ ODD_Sum.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,a[100],os=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        {
+            os=os+a[i];
+        }
+    }
+    printf("%d",os);
+}
