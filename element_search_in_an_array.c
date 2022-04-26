3128a7eabaf03768bec6a89817cd2b7b6fce53c5
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-04-27 00:01:53 +0530

    #include<stdio.h> int main() {     int c=0,n,a[100],i,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(a[i]==k)         {             c++;         }     }     if(c>0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int c=0,n,a[100],i,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            c++;
+        }
+    }
+    if(c>0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
+
