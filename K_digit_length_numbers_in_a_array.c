00cdd8889171dc25e828efb3cc18b186dd2391f8
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-05-23 11:30:32 +0530

    #include<stdio.h> int main() {     int f,n,a[100],i,d,c=0,k,s,r=0;     scanf("%d%d",&n,&f);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         k=a[i];         if(k!=0)         {         while(k)         {             d=k%10;             c++;             k=k/10;         }         }         else         {             c=1;         }         s=c;         {             c=0;         }         if(s==f)         {             r++;         }     }     printf("%d",r); }

---
+++ K_digit_length_numbers_in_a_array.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int f,n,a[100],i,d,c=0,k,s,r=0;
+    scanf("%d%d",&n,&f);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        k=a[i];
+        if(k!=0)
+        {
+        while(k)
+        {
+            d=k%10;
+            c++;
+            k=k/10;
+        }
+        }
+        else
+        {
+            c=1;
+        }
+        s=c;
+        {
+            c=0;
+        }
+        if(s==f)
+        {
+            r++;
+        }
+    }
+    printf("%d",r);
+}
