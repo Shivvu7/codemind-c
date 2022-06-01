77534774302d0977918feb646991195d63c36f51
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-06-01 20:24:27 +0530

    // strictly even #include<stdio.h> int main() { 	int a; 	scanf("%d",&a); 	int arr[a],i,j,flag=0; 	for (i=0;i<a; i++) 	{ 		scanf("%d",&arr[i]); 	} 	for (i=0; i<a; i++) 	{ 		if (i%2==0 && arr[i]%2==0) 		{ 			flag=1; 		} 		else if (i%2!=0 && arr[i]%2==0) 		{ 			flag=0; 			break; 		} 	} 	if (flag==1) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} 	return 0; }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,33 @@
+// strictly even
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	int arr[a],i,j,flag=0;
+	for (i=0;i<a; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for (i=0; i<a; i++)
+	{
+		if (i%2==0 && arr[i]%2==0)
+		{
+			flag=1;
+		}
+		else if (i%2!=0 && arr[i]%2==0)
+		{
+			flag=0;
+			break;
+		}
+	}
+	if (flag==1)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+	return 0;
+}
