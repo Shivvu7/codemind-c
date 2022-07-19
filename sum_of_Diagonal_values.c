4eb460cdfc6a1a0b51a5b17c2b8a02fe7ba6c260
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-07-19 10:04:52 +0530

    #include<stdio.h> int main() {     int a[100][100],i,j,m,n,s=0;     scanf("%d%d",&m,&n);     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);             if(i==j)             {                 s+=a[i][j];             }         }         if (i!=n-i-1)         {         s+=a[i][n-i-1];         }     }     printf("%d",s); }

--- sum_of_Diagonal_values.c
+++ sum_of_Diagonal_values.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a[100][100],m,n,i,j,s=0;
+    int a[100][100],i,j,m,n,s=0;
     scanf("%d%d",&m,&n);
     for(i=0;i<m;i++)
     {
@@ -13,9 +13,9 @@ int main()
                 s+=a[i][j];
             }
         }
-        if(i!=m-1-i)
+        if (i!=n-i-1)
         {
-        s+=a[i][m-1-i];
+        s+=a[i][n-i-1];
         }
     }
     printf("%d",s);
